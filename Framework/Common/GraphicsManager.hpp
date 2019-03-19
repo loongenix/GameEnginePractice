346a163247ac3f6cecced1eb32d4835373452f9d
jeremy <jijiachun@apusapps.com>
2019-03-19 22:06:32 +0800

    文章7,8

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+namespace Engine {
+
+class GraphicsManager : public IRuntimeModule {
+ private:
+  /* data */
+ public:
+  virtual ~GraphicsManager() {}
+};
+}  // namespace Engine
